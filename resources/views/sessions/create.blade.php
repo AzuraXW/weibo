5afee27c0ea9f9d03bd8f3d8598fbec585d56c98
AzuraXW <2219444825@qq.com>
2021-03-16 15:03:12 +0800

    创建会话

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.default')
+@section('title', '登录')
+
+@section('content')
+<div class="offset-md-2 col-md-8">
+  <div class="card">
+    <div class="card-header">
+      <h5>登录</h5>
+    </div>
+    <div class="card-body">
+      @include('shared._errors')
+      <form action="{{route('login')}}" method="post">
+        {{csrf_field()}}
+        <div class="form-group">
+          <label for="email">邮箱：</label>
+          <input type="text" class="form-control" name="email" value="{{old('email')}}">
+        </div>
+        <div class="form-group">
+          <label for="email">密码：</label>
+          <input type="password" class="form-control" name="password" value="{{old('password')}}">
+        </div>
+        <button type="submit" class="btn btn-primary">登录</button>
+      </form>
+      <hr>
+      <p>还没注册？<a href="{{route('singup')}}">现在注册</a></p>
+    </div>
+  </div>
+</div>
+@stop
